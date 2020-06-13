b24c5910463453e14be965893cd2a50859a945dd
lowercasename <raphaelkabo@gmail.com>
2020-06-13 13:36:28 +0100

    Added Nodemailer as an SMTP service

--- config/domain-example.js
+++ config/domain-example.js
@@ -3,6 +3,7 @@ module.exports = {
     'domain' : 'localhost:3000' ,
     'port': '3000',
     'email': 'contact@example.com',
+    'mailService': 'nodemailer', // Which mail service to use to send emails to attendees. Options are 'nodemailer' or 'sendgrid'. Configure settings for the mail service in config/api.js.z
     'sitename': 'gathio',
     'isFederated': true,
     // If left blank, this defaults to https://yourdomain.com/images/gathio-email-logo.gif. Set a full URL here to change it to your own logo (or just change the file itself)
