98cbc016ddf18a4f198330ef090b316e50950724
Raphael Kabo <raphaelkabo@gmail.com>
2022-01-17 10:41:07 +0000

    Set up localStorage functionality and frontend

---
+++ public/js/util.js
@@ -0,0 +1,34 @@
+const getStoredToken = function(eventID) {
+    try {
+        let editTokens = JSON.parse(localStorage.getItem('editTokens'));
+        return editTokens[eventID];
+    } catch(e) {
+        console.error(e);
+        localStorage.setItem('editTokens', JSON.stringify({}));
+        return false;
+    }
+}
+
+const addStoredToken = function(eventID, token) {
+    try {
+        let editTokens = JSON.parse(localStorage.getItem('editTokens'));
+        editTokens[eventID] = token;
+        localStorage.setItem('editTokens', JSON.stringify(editTokens));
+    } catch(e) {
+        console.error(e);
+        localStorage.setItem('editTokens', JSON.stringify({ [eventID]: token }));
+        return false;
+    }
+} 
+
+const removeStoredToken = function(eventID) {
+    try {
+        let editTokens = JSON.parse(localStorage.getItem('editTokens'));
+        delete editTokens[eventID];
+        localStorage.setItem('editTokens', JSON.stringify(editTokens));
+    } catch(e) {
+        console.error(e);
+        localStorage.setItem('editTokens', JSON.stringify({}));
+        return false;
+    }
+}
