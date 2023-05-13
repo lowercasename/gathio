45ecdf59f8b0c4090041ce86aad2e5f606253f0d
Raphael Kabo <mail@raphaelkabo.com>
2023-05-13 19:19:37 +0100

    Migrate config library to TOML

--- src/config/api-example.js
+++
@@ -1,8 +0,0 @@
-// Which of these fields are used depends on the 'mailService' config entry in config/domain.js
-module.exports = {
-  sendgrid: "", // If using SendGrid, the Sendgrid API key goes here
-  smtpServer: "", // If using Nodemailer, your SMTP server hostname goes here
-  smtpPort: "", // If using Nodemailer, your SMTP server port goes here
-  smtpUsername: "", // If using Nodemailer, your SMTP server username goes here
-  smtpPassword: "", // If using Nodemailer, your SMTP password goes here
-};
