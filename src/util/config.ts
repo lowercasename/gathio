44cd1d5b4ee6bfbccffbf6c2448b684a7afd31de
Raphael Kabo <raphaelkabo@hey.com>
2023-10-06 16:17:03 +0100

    Add config util file

---
+++ src/util/config.ts
@@ -0,0 +1,17 @@
+import getConfig from "../lib/config.js";
+
+const config = getConfig();
+
+interface FrontendConfig {
+  domain: string;
+  email: string;
+  siteName: string;
+  showKofi: boolean;
+}
+
+export const frontendConfig = (): FrontendConfig => ({
+  domain: config.general.domain,
+  email: config.general.email,
+  siteName: config.general.site_name,
+  showKofi: config.general.show_kofi,
+});
