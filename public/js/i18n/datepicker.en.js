e3bdcdfc6222a114b6132ff193a15749037b2156
ligi <ligi@ligi.de>
2024-02-09 02:32:04 +0100

    Fix date format

--- public/js/i18n/datepicker.en.js
+++ public/js/i18n/datepicker.en.js
@@ -6,7 +6,7 @@
     monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
     today: 'Today',
     clear: 'Clear',
-    dateFormat: 'mm/dd/yyyy',
+    dateFormat: 'dd-mm-yyyy',
     timeFormat: 'hh:ii aa',
     firstDay: 0
 }; })(jQuery);
